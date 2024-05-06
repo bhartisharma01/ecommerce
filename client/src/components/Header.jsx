096ac3cce6f1d2982ff41e64e5827f4843ef5530
bhartisharma <bharti.s@binmile.com>
2024-05-06 16:15:21 +0530

    added link to the logo and login button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { MdSearch } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { IoCart } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="flex justify-between shadow-md items-center">
+    <header className="flex justify-between shadow-md items-center bg-white">
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
         <div>
-          <img
-            src="src/assets/logo.jpg"
-            alt="Logo"
-            className="logo w-30 h-20"
-          />
+          <Link to={"/"}>
+            <img
+              src="src/assets/logo.jpg"
+              alt="Logo"
+              className="logo w-30 h-20"
+            />
+          </Link>
         </div>
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
           <input
@@ -38,12 +41,11 @@ const Header = () => {
             </div>
           </div>
           <div>
-            <button
-              type="button"
+            <Link to={'/login'}
               className=" bg-blue text-white px-3 py-1 rounded-full hover:bg-orange"
             >
               Login
-            </button>
+            </Link>
           </div>
         </div>
       </div>
